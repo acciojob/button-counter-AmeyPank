fc16d52040aa50919ac8ba9107c5bbe36729a025
Amey  Pankanti <pankantiameya@gmail.com>
2023-05-11 22:52:16 +0530

    push

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,14 @@ import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0); // take count useState
+  function countClick(){
+    setCount(count+1);
+  }
   return (
     <div>
-      <p>Button clicked {count} times </p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
+      <p> Button clicked {count} times</p>
+      <button onClick={countClick}>Click me</button>
     </div>
   )
 }
