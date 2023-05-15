5528cff2dec923ed4f6b689c45255bf16a577a76
AmeyPank <pankantiameya@gmail.com>
2023-05-16 00:49:37 +0530

    Submitting

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ const App = () => {
   }
   return (
     <div>
-      <p> Button clicked {count} times</p>
+      <p>Button clicked {count} times</p>
       <button onClick={countClick}>Click me</button>
     </div>
   )
